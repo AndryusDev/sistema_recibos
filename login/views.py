035d543beb1e1a628cce5977270de76d4c7ef31e
AndryusDev <andryusdev@gmail.com>
2025-03-22 20:06:14 -0400

    Conectado los componentes compatidos para todas las pantallas (header y footes)

--- login/views.py
+++ login/views.py
@@ -6,3 +6,6 @@ def login(request):
     enable_fields = {'campo1': True, 'campo2': False}
     enable_fields_json = json.dumps(enable_fields)
     return render(request, 'login.html', {'enable_fields_json': enable_fields_json})
+
+def crear_cuenta(request):
+    return render(request, 'crear_cuenta.html')
