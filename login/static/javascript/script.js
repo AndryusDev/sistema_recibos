bdfb0379a9968ac95da78e9577521b86ba49882f
AndryusDev <andryusdev@gmail.com>
2025-03-28 03:00:39 -0400

    Agragando movimiento a los 3 formularios

--- login/static/javascript/script.js
+++ login/static/javascript/script.js
@@ -1,8 +1,15 @@
+
 const formulario__boton__autenticacion = document.getElementById("formulario__boton__autenticacion");
-const formulario__contenedor = document.querySelector(".formulario__contenedor")
+const formulario__boton__crearcuenta = document.getElementById("formulario__boton__crearcuenta");
+const formulario__boton__preguntasseguridad = document.getElementById("formulario__boton__preguntasseguridad");
+const formulario__contenedor = document.querySelector(".formulario__contenedor");
 
-formulario__boton__autenticacion.addEventListener("click",()=>{
-    formulario__contenedor.classList.toggle("toggle");
+// Mueve el formulario a la izquierda cuando presionas "Siguiente"
+formulario__boton__autenticacion.addEventListener("click", () => {
+    formulario__contenedor.classList.add("toggle");
+});
 
-
+ // Mueve el formulario aún más a la izquierda cuando presionas "Crear Cuenta"
+formulario__boton__crearcuenta.addEventListener("click", () => {
+    formulario__contenedor.classList.add("toggle-2");
 });
