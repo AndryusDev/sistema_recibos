5276eab2d372518a6746a34967e557730b5c874f
AndryusDev <andryusdev@gmail.com>
2025-03-28 01:34:45 -0400

    pruebas con el la funcionalidad del boton siguiente de autenticacion de usuario

---
+++ login/static/javascript/script.js
@@ -0,0 +1,8 @@
+const formulario__boton__autenticacion = document.getElementById("formulario__boton__autenticacion");
+const formulario__contenedor = document.querySelector(".formulario__contenedor")
+
+formulario__boton__autenticacion.addEventListener("click",()=>{
+    formulario__contenedor.classList.toggle("toggle");
+
+
+});
