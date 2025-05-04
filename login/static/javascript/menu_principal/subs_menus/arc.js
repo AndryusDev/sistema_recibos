16c617738a2b3c9aad5ba8c37d748befc3ccca38
AndryusDev <andryusdev@gmail.com>
2025-05-04 02:40:36 -0400

    arreglando responsividad y estructura de la tabla modal_arc

--- login/static/javascript/menu_principal/subs_menus/arc.js
+++ login/static/javascript/menu_principal/subs_menus/arc.js
@@ -31,26 +31,65 @@ function arcModal__abrir(anio){
             </div>
         </div>
 
-        <div>
+        <div class="sub">
             <h3 class="sub_titulo">ARC - AÑO ${anio}</h3>
         </div>
+        
+        <div class="contenedor-tablas">
+            <table class="tabla-modal-arc__agente">
+                <thead>
+                    <tr>
+                        <th>NOMBRE O RAZÓN SOCIAL DEL AGENTE DE RETENCIÓN</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>POLICÍA BOLIVARIANA DEL ESTADO ANZOÁTEGUI</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="tabla-modal-arc__sujeto">
+                <thead>
+                    <tr>
+                        <th>NOMBRE O RAZÓN SOCIAL DEL SUJETO RETENCIÓN</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>ANDRYUS JOSÉ GUAIQUIRIMA CERMEÑO</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-        <br>
-
-        <table class="tabla-modal-arc">
-            <tr>
-                <td><strong>NOMBRE O RAZÓN SOCIAL DEL AGENTE DE RETENCIÓN:</strong><br>POLICÍA BOLIVARIANA DEL ESTADO ANZOÁTEGUI</td>
-                <td><strong>NOMBRE O RAZÓN SOCIAL DEL SUJETO RETENIDO:</strong><br>SORELYS DEL VALLE MAIGUA DÍAZ</td>
-            </tr>
-            <tr>
-                <td colspan="2"><strong>DIRECCIÓN DEL AGENTE DE RETENCIÓN:</strong><br>Av. Jorge Rodríguez, Crucero de Lechería, Coordinación Policial G/D José Antonio Anzoátegui de la Policía Bolivariana del Estado Anzoátegui</td>
-            </tr>
-            <tr>
-                <td colspan="2"><strong>NRO. DE CÉDULA DEL SUJETO RETENIDO:</strong> 8260027</td>
-            </tr>
-        </table>
-
-        <br>
+        <div class="contenedor-tablas2">
+            <table class="tabla-modal-arc__agente__direccion">
+                <thead>
+                    <tr>
+                        <th>DIRECCIÓN DEL AGENTE DE RETENCIÓN</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="minimizar">Av. Jorge Rodríguez, Crucero de Lechería, Coordinación Policial G/D <br>
+                        Jose Antonio Anzoátegui de la Policía Bolivariana del Estado <br>
+                        Anzoátegui</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="tabla-modal-arc__sujeto__cedula">
+                <thead>
+                    <tr>
+                        <th>NRO. DE CEDULA DEL SUJETO RETENIDO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>30.480.815</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
         <table class="tabla-arc-detalle">
             <thead>
@@ -492,20 +531,49 @@ function arcModal__abrir(anio){
                     </td>
                     <td>0,00</td>
                 </tr>
+                <tr class="fila-total">
+                    <!-- Celda que abarca las primeras 6 columnas -->
+                    <td colspan="7" class="total-texto">Total monto a declarar Bs.:</td>
+                    
+                    <!-- Celda para el monto (debajo de "monto a declarar") -->
+                    <td class="total-monto">9.243,62</td>
+                </tr>
             </tbody>
         </table>
-
-        <p class="total-declarar">Total monto a declarar Bs.: 9.243,62</p>
-
-        <table class="tabla-arc-resumen">
-            <tr>
-                <td><strong>VACACIONES</strong><br>5.716,02</td>
-                <td><strong>AGUINALDOS</strong><br>0,00</td>
-                <td><strong>EVALUACIÓN</strong><br>0,00</td>
-                <td><strong>SALARIOS</strong><br>3.527,60</td>
-            </tr>
-        </table>
-
+        <div class="tabla-arc-resumen">
+            <table class="tabla-arc-resumen__vacaciones">
+                <tr>
+                    <th><strong>VACACIONES</strong></th>
+                </tr>
+                <tr>
+                    <td>5.716,02</td>
+                </tr>
+            </table>
+            <table class="tabla-arc-resumen__aguinaldos">
+                <tr>
+                    <th><strong>AGUINALDOS</strong></th>
+                </tr>
+                <tr>
+                    <td>0,00</td>
+                </tr>
+            </table>
+            <table class="tabla-arc-resumen__evaluacion">
+                <tr>
+                    <th><strong>EVALUACIÓN</strong></th>
+                <tr>
+                <tr>
+                    <td>0,00</td>
+                <tr>
+            </table>
+            <table class="tabla-arc-resumen__salarios">
+                <tr>
+                    <th><strong>SALARIOS</strong></th>
+                </tr>
+                <tr>
+                    <td>3.527,60</td>
+                </tr>
+            </table>
+        </div>
         <p class="nota-final">Individuo no trabajador</p>
     </div>
     `;
