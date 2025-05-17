709cd6854761956e959da92f55184bf2e6d03215
AndryusDev <andryusdev@gmail.com>
2025-05-16 23:37:23 -0400

    eliminando espacios demas

--- login/views.py
+++ login/views.py
@@ -1,8 +1,5 @@
-from sqlite3 import IntegrityError
 import json
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework_simplejwt.tokens import RefreshToken
